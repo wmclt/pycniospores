e75e91b6d08c39e78385f318b27669176d456513
William Mclt <william.mauclet+gitlab@gmail.com>
2020-10-17 18:16:17 +0200

    It works, but still pretty primitive.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ extern crate opengl_graphics;
 extern crate piston;
 extern crate rand;
 
-use rand::{distributions::Standard, prelude::*};
+pub mod spore;
+
 use std::f64::consts::PI;
 
 use glutin_window::GlutinWindow;
 use graphics::{circle_arc, clear, color::hex, Context, Transformed};
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::{EventSettings, Events, RenderArgs, RenderEvent, WindowSettings};
+use spore::{generate_spores, move_spores, Spore, SporeType, WINDOW_HEIGHT, WINDOW_WIDTH};
 
 // TODO rename UNIVERSE_HEIGHT and UNIVERSE_WIDTH
-const WINDOW_HEIGHT: f64 = 800.0;
-const WINDOW_WIDTH: f64 = 1200.0;
 
 const DIAMETER: f64 = 5.0;
 
@@ -29,41 +29,50 @@ fn main() {
     let mut app = App {
         gl: GlGraphics::new(opengl),
         tick: 0,
-        spores: generate_spores(),
+        spores: generate_spores(WINDOW_WIDTH, WINDOW_HEIGHT),
     };
 
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
+        // TODO resizing
+        // if let Some(Button::Keyboard(key)) = e.press_args() {
+        //     match key {
+        //         piston::Key::Down => {
+        //         }
+        //         piston::Key::Up => {}
+        //         _ => {}
+        //     }
+        // }
+
         if let Some(r) = e.render_args() {
-            // TODO exercise forces
-            // TODO modify speed based on force - wrijving
+            // TODO might be useful for window resizing: see https://github.com/amarao/piston_play/blob/e42099ae7dadff377e029af1703512f7765756da/src/main.rs
+            // if r.draw_size[0] != x || r.draw_size[1] != y {
+            //     let (draw_tx, draw_rx)= mpsc::sync_channel(128);
 
+            //     let new_x = r.draw_size[0];
+            //     let new_y = r.draw_size[1];
+            //     println!("Resolution change from {}x{} to {}x{}", x, y, new_x, new_y);
+            //     control_tx.send(ControlCommand{command: Command::NewResolution(new_x, new_y)}).unwrap();
+            //     while let Ok(_command) = draw_rx.try_recv(){}
+            //     buf = scale(buf, x, y, new_x, new_y);
+            //     x = new_x;
+            //     y = new_y;
+            //     control_tx.send(ControlCommand{command:Command::Continue}).unwrap();
+            //     texture = Texture::from_image(
+            //         &mut texture_context,
+            //         &buf,
+            //         &TextureSettings::new()
+            //     ).unwrap();
+            // }
+            app.tick += 1;
             move_spores(&mut app.spores);
-
             app.render(&r);
         }
     }
 }
 
-fn move_spores(spores: &mut Vec<Spore>) {
-    for spore in spores {
-        spore.x_coord = (spore.x_coord + spore.x_speed) % WINDOW_WIDTH;
-        spore.y_coord = (spore.y_coord + spore.y_speed) % WINDOW_HEIGHT;
-    }
-}
-
 pub struct App {
     gl: GlGraphics,
-    // left_score: i32,
-    // left_pos: i32,
-    // left_vel: i32,
-    // right_score: i32,
-    // right_pos: i32,
-    // right_vel: i32,
-    // ball_x: i32,
-    // ball_y: i32,
-    // vel_x: i32,
-    // vel_y: i32,
     tick: u64,
     spores: Vec<Spore>,
 }
@@ -71,15 +80,15 @@ pub struct App {
 impl App {
     fn render(&mut self, args: &RenderArgs) {
         let background: [f32; 4] = hex("000033");
-
         let spores = &self.spores;
         self.gl.draw(args.viewport(), |c, gl| {
             clear(background, gl);
 
+            // TODO show tick as text
+
             for spore in spores {
                 draw_spore(c, gl, spore.spore_type, spore.x_coord, spore.y_coord);
             }
-
             // rectangle(FOREGROUND, rectangle::square(0.0, 0.0, 50.0), c.transform.trans(1.0, 1.0), gl);
         });
     }
@@ -97,87 +106,18 @@ fn draw_spore(c: Context, gl: &mut GlGraphics, sp: SporeType, x: f64, y: f64) {
     );
 }
 
-// colours: [red, green, blue, alpha]
-
-// TODO colour field
-#[derive(Debug, Copy, Clone)]
-pub enum SporeType {
-    One,
-    Two,
-    Three,
-    Four,
-    Five,
-}
 // https://www.color-hex.com/color-palette/99210
-fn get_color(sp: SporeType) -> [f32; 4] {
+pub fn get_color(sp: SporeType) -> [f32; 4] {
     match sp {
         // SporeType::One => [1.00, 1.00, 0.95, 1.0],
-        SporeType::One => hex("bd000a"),
+        SporeType::One => hex("BD000A"),
         // SporeType::Two => [0.04, 1.00, 0.42, 1.0],
         SporeType::Two => hex("F85E00"),
         // SporeType::Three => [0.08, 1.00, 0.50, 1.0],
         SporeType::Three => hex("1A936F"),
         // SporeType::Four => [0.11, 1.00, 0.50, 1.0],
-        SporeType::Four => hex("ffa700"),
+        SporeType::Four => hex("FFA700"),
         // SporeType::Five => [0.57, 0.31, 0.72, 1.0],
-        SporeType::Five => hex("a2bbce"),
-    }
-}
-
-pub struct Spore {
-    x_coord: f64,
-    y_coord: f64,
-    x_speed: f64,
-    y_speed: f64,
-    spore_type: SporeType,
-}
-
-fn generate_spores() -> Vec<Spore> {
-    let mut results = Vec::new();
-    let mut rng = rand::thread_rng();
-
-    for _ in 0..100 {
-        let x_coord: f64 = rng.gen_range(0.0, WINDOW_WIDTH);
-        let y_coord: f64 = rng.gen_range(0.0, WINDOW_HEIGHT);
-        let x_speed: f64 = rng.gen_range(-1.0, 1.0);
-        let y_speed: f64 = rng.gen_range(-1.0, 1.0);
-
-        results.push(new_spore(
-            x_coord,
-            y_coord,
-            x_speed,
-            y_speed,
-            rand::random(),
-        ));
-    }
-    results
-}
-
-impl Distribution<SporeType> for Standard {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> SporeType {
-        match rng.gen_range(0, 4) {
-            0 => SporeType::One,
-            1 => SporeType::Two,
-            2 => SporeType::Three,
-            3 => SporeType::Four,
-            _ => SporeType::Five,
-        }
-    }
-}
-
-// TODO spores will start with speed 0
-fn new_spore(
-    x_coord: f64,
-    y_coord: f64,
-    x_speed: f64,
-    y_speed: f64,
-    spore_type: SporeType,
-) -> Spore {
-    Spore {
-        x_coord,
-        y_coord,
-        x_speed,
-        y_speed,
-        spore_type,
+        SporeType::Five => hex("A2BBCE"),
     }
 }
---
+++ src/spore.rs
@@ -0,0 +1,255 @@
+use rand::{distributions::Standard, prelude::*};
+
+const REPULSION_DISTANCE: f64 = 10.0;// FORCE_REACH / 8.0; 
+const REPULSION_AMPLITUDE: f64 = -0.05 * DEFAULT_FORCE_AMPLITUDE;
+
+const FORCE_REACH: f64 = 70.0;
+// const FORCE_REACH_SQUARED: f64 = FORCE_REACH * FORCE_REACH;
+const DEFAULT_FORCE_AMPLITUDE: f64 = 0.005; //0.01 is pretty good!
+
+const FRICTION: f64 = 0.9875; // friction should be low!
+
+pub const WINDOW_HEIGHT: f64 = 800.0;
+pub const WINDOW_WIDTH: f64 = 1200.0;
+
+const NUMBER_OF_SPORES: u16 = 300;
+
+const ZERO_FORCE: f64 = 0.0 * DEFAULT_FORCE_AMPLITUDE;
+const ONE_THIRD_FORCE: f64 = 0.33 * DEFAULT_FORCE_AMPLITUDE;
+const TWO_THIRDS_FORCE: f64 = 0.66 * DEFAULT_FORCE_AMPLITUDE;
+const FULL_FORCE: f64 = 1.0 * DEFAULT_FORCE_AMPLITUDE;
+const MINUS_HALF_FORCE: f64 = 0.5 * DEFAULT_FORCE_AMPLITUDE;
+const MINUS_FULL_FORCE: f64 = -0.9 * DEFAULT_FORCE_AMPLITUDE;
+
+
+pub struct Spore {
+    //TODO
+    pub id: u16,
+    pub x_coord: f64,
+    pub y_coord: f64,
+    x_speed: f64,
+    y_speed: f64,
+    pub spore_type: SporeType,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub enum SporeType {
+    One,   // aka moving glue
+    Two,   // aka non-moving glue
+    Three, // aka moving antiglue
+    Four,  // aka non-moving antiglue
+    Five,  // aka does nothing
+}
+
+pub fn generate_spores(max_x: f64, max_y: f64) -> Vec<Spore> {
+    let mut results = Vec::new();
+    let mut rng = rand::thread_rng();
+
+    for id in 0..NUMBER_OF_SPORES {
+        //100
+        let x_coord: f64 = rng.gen_range(0.0, max_x);
+        let y_coord: f64 = rng.gen_range(0.0, max_y);
+        // let x_speed: f64 = rng.gen_range(-1.0, 1.0);
+        // let y_speed: f64 = rng.gen_range(-1.0, 1.0);
+        let x_speed: f64 = 0.0;
+        let y_speed: f64 = 0.0;
+
+        results.push(new_spore(
+            id,
+            x_coord,
+            y_coord,
+            x_speed,
+            y_speed,
+            rand::random(),
+        ));
+    }
+    results
+}
+
+impl Distribution<SporeType> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> SporeType {
+        match rng.gen_range(0, 4) {
+            0 => SporeType::One,
+            1 => SporeType::Two,
+            2 => SporeType::Three,
+            3 => SporeType::Four,
+            _ => SporeType::Five,
+        }
+    }
+}
+
+// TODO spores will start with speed 0
+pub fn new_spore(
+    id: u16,
+    x_coord: f64,
+    y_coord: f64,
+    x_speed: f64,
+    y_speed: f64,
+    spore_type: SporeType,
+) -> Spore {
+    Spore {
+        id,
+        x_coord,
+        y_coord,
+        x_speed,
+        y_speed,
+        spore_type,
+    }
+}
+
+pub fn move_spores(spores: &mut Vec<Spore>) {
+    // TODO
+    // ONE single loop
+    // 1. find neighbours close enough
+    //      a. square filter
+    //      b. round filter
+    // 2. calculate forces
+
+    // 3. update speeds: apply forces + friction to speeds
+    // 4. move according to speed
+
+    // TODO remove
+    // for spore in spores.iter() {
+    //     // let neighbours =
+    //     calculate_forces(&spore, &spores); // TODO use unsafe?
+    //                                        // spore.x_coord = (spore.x_coord + spore.x_speed) % WINDOW_WIDTH;
+    //                                        // spore.y_coord = (spore.y_coord + spore.y_speed) % WINDOW_HEIGHT;
+    // }
+
+    let forces: Vec<Force> = spores
+        .iter()
+        .map(|spore| calculate_forces(&spore, &spores))
+        .collect();
+
+    spores
+        .iter_mut()
+        .zip(forces.iter())
+        .for_each(|(spore, force)| apply_force_and_move(spore, force));
+}
+
+pub struct Dist {
+    x_dist: f64,
+    y_dist: f64,
+    tot_dist: f64,
+}
+
+pub struct Force {
+    x_force: f64,
+    y_force: f64,
+}
+
+pub fn calculate_forces(spore: &Spore, spores: &Vec<Spore>) -> Force {
+    spores
+        .iter()
+        .filter(|&other| spore.id != other.id)
+        .filter(|&other| {
+            (other.x_coord - spore.x_coord).abs() <= FORCE_REACH
+                && (other.y_coord - spore.y_coord).abs() <= FORCE_REACH
+        })
+        .map(|other| {
+            let x_dist = other.x_coord - spore.x_coord;
+            let y_dist = other.y_coord - spore.y_coord;
+            let tot_dist = (x_dist.powi(2) + y_dist.powi(2)).sqrt();
+            (
+                other,
+                Dist {
+                    x_dist,
+                    y_dist,
+                    tot_dist,
+                },
+            )
+        })
+        .filter(|(_other, dist)| dist.tot_dist <= FORCE_REACH)
+        .map(|(other, dist)| calculate_force(other.spore_type, spore.spore_type, dist))
+        // .map(|(other, dist)| scale_force(5.0, 1.0, 1.0))
+        .fold(zero_force(), |force, next_force| Force {
+            x_force: force.x_force + next_force.x_force,
+            y_force: force.y_force + next_force.y_force,
+        })
+}
+
+fn zero_force() -> Force {
+    Force {
+        x_force: 0.0,
+        y_force: 0.0,
+    }
+}
+
+// TODO the force is linear to the distance
+// TODO maybe try with different functions
+// TODO also try different distance functions?
+fn scale_force(factor: f64, x_dist: f64, y_dist: f64) -> Force {
+    Force {
+        x_force: x_dist * factor,
+        y_force: y_dist * factor,
+    }
+}
+
+// from <right> on <down>	    One	Two	Three	Four	Five
+//                      One	    0	0.66	1	0	-0.5
+//                      Two	    0.66	-1	-0.5	0	-0.5
+//                      Three	1	0.33	1	0.33	0.66
+//                      Four	-0.5	1	0.33	0.66	0.33
+//                      Five	0.66	0.33	-0.5	0.66	1
+pub fn calculate_force(other: SporeType, spore: SporeType, dist: Dist) -> Force {
+    // if too close: acceleration away
+    if dist.tot_dist < REPULSION_DISTANCE {
+        return Force {
+            x_force: 1.0 / ((dist.x_dist).abs()-REPULSION_DISTANCE/2.0).exp2() * REPULSION_AMPLITUDE * dist.x_dist.signum() + 0.5 * dist.x_dist.signum() ,
+            y_force: 1.0 / ((dist.y_dist).abs()-REPULSION_DISTANCE/2.0).exp2() * REPULSION_AMPLITUDE * dist.y_dist.signum()+ 0.5 * dist.y_dist.signum(),
+        };
+    }
+
+    let force_factor: f64 = match spore {
+        SporeType::One => match other {
+            SporeType::One => ZERO_FORCE,
+            SporeType::Two => TWO_THIRDS_FORCE,
+            SporeType::Three => FULL_FORCE,
+            SporeType::Four => ZERO_FORCE,
+            SporeType::Five => MINUS_HALF_FORCE,
+        },
+        SporeType::Two => match other {
+            SporeType::One => TWO_THIRDS_FORCE,
+            SporeType::Two => MINUS_FULL_FORCE,
+            SporeType::Three => MINUS_HALF_FORCE,
+            SporeType::Four => ZERO_FORCE,
+            SporeType::Five => MINUS_HALF_FORCE,
+        },
+        SporeType::Three => match other {
+            SporeType::One => FULL_FORCE,
+            SporeType::Two => ONE_THIRD_FORCE,
+            SporeType::Three => FULL_FORCE,
+            SporeType::Four => ONE_THIRD_FORCE,
+            SporeType::Five => TWO_THIRDS_FORCE,
+        },
+        SporeType::Four => match other {
+            SporeType::One => MINUS_HALF_FORCE,
+            SporeType::Two => FULL_FORCE,
+            SporeType::Three => ONE_THIRD_FORCE,
+            SporeType::Four => TWO_THIRDS_FORCE,
+            SporeType::Five => ONE_THIRD_FORCE,
+        },
+        SporeType::Five => match other {
+            SporeType::One => TWO_THIRDS_FORCE,
+            SporeType::Two => ONE_THIRD_FORCE,
+            SporeType::Three => MINUS_HALF_FORCE,
+            SporeType::Four => TWO_THIRDS_FORCE,
+            SporeType::Five => FULL_FORCE,
+        },
+    };
+    scale_force(force_factor, dist.x_dist, dist.y_dist)
+}
+
+fn apply_force_and_move(spore: &mut Spore, force: &Force) {
+    spore.x_speed = spore.x_speed * FRICTION + force.x_force;
+    spore.y_speed = spore.y_speed * FRICTION + force.y_force;
+
+    let new_x_coord = spore.x_coord + spore.x_speed;
+    let new_y_coord = spore.y_coord + spore.y_speed;
+
+    spore.x_coord = (((new_x_coord) % WINDOW_WIDTH) + WINDOW_WIDTH) % WINDOW_WIDTH;
+    spore.y_coord = ((new_y_coord % WINDOW_HEIGHT) + WINDOW_HEIGHT) % WINDOW_HEIGHT;
+
+    // print!("coords: ( {} , {} )", spore.x_coord, spore.y_coord);
+    // print!("speeds: ( {} , {} )", spore.x_speed, spore.y_speed);
+}
