01c87a2624c30402cea0ca53991646fa95d2714e
William Mclt <william.mauclet+gitlab@gmail.com>
2021-04-16 18:49:06 +0200

    Small refactor in spore_mover.rs

--- src/spore_mover.rs
+++ src/spore_mover.rs
@@ -13,43 +13,19 @@ use crate::{
 //  2. apply forces
 //      3. update speeds: apply forces + friction to speeds
 //      4. move according to speed
-pub fn move_spores(spore_configs: &SporeConfigs, spores: &mut SporesState) {
+pub fn move_spores(spore_configs: &SporeConfigs, spores_state: &mut SporesState) {
     for vert in 0..NBR_VERT_BUCKETS {
         for horz in 0..NBR_HORZ_BUCKETS {
-            move_spores_in_bucket(spore_configs, spores, (horz as usize, vert as usize));
+            move_spores_in_bucket(spore_configs, spores_state, (horz as usize, vert as usize));
         }
     }
 
     for vert in 0..NBR_VERT_BUCKETS {
         for horz in 0..NBR_HORZ_BUCKETS {
-            let bucket_movements = calc_bucket_movements(&spores, horz, vert);
-
-            // copy spores to new bucket TODO: sort to_move by bucket?
-            fun_name(&bucket_movements, spores);
-
-            // remove spores that have moved (=only keep spores that haven't moved) [uses bad magic :/]
-            let bucket_movement: Vec<usize> = bucket_movements
-                .iter()
-                .map(|movement| movement.old_bucket_index)
-                .collect();
-            let mut i: usize = 0;
-            spores.positions[vert][horz].retain(|_| (!bucket_movement.contains(&i), i += 1).0);
-            i = 0;
-            spores.speeds[vert][horz].retain(|_| (!bucket_movement.contains(&i), i += 1).0);
-            i = 0;
-            spores.spore_types[vert][horz].retain(|_| (!bucket_movement.contains(&i), i += 1).0);
+            let bucket_movements = calc_bucket_movements(&spores_state, horz, vert);
+            copy_spores_to_new_bucket(&bucket_movements, spores_state);
+            remove_spores_from_old_buckets(&bucket_movements, spores_state, vert, horz);
         }
-    }
-}
-
-fn fun_name(bucket_movements: &Vec<SporeBucketMovement>, spores: &mut SporesState) {
-    for movement in bucket_movements {
-        let (new_horz, new_vert) = movement.new_bucket_coord;
-        let (pos, speed, spore_type) = movement.spore_data;
-
-        spores.positions[new_vert][new_horz].push(pos);
-        spores.speeds[new_vert][new_horz].push(speed);
-        spores.spore_types[new_vert][new_horz].push(spore_type);
     }
 }
 
@@ -68,8 +44,34 @@ fn move_spores_in_bucket(
     }
 }
 
+fn copy_spores_to_new_bucket(bucket_movements: &Vec<SporeBucketMovement>, spores: &mut SporesState) {
+    for movement in bucket_movements {
+        let (new_horz, new_vert) = movement.new_bucket_coord;
+        let (pos, speed, spore_type) = movement.spore_data;
+
+        spores.positions[new_vert][new_horz].push(pos);
+        spores.speeds[new_vert][new_horz].push(speed);
+        spores.spore_types[new_vert][new_horz].push(spore_type);
+    }
+}
+
+/// remove spores that have moved (=only keep spores that haven't moved) (uses black magic :/)
+fn remove_spores_from_old_buckets(bucket_movements: &Vec<SporeBucketMovement>, spores: &mut SporesState, vert: usize, horz: usize) {
+    let bucket_movement: Vec<usize> = bucket_movements
+        .iter()
+        .map(|movement| movement.index_in_old_bucket)
+        .collect();
+    let mut i: usize = 0;
+    spores.positions[vert][horz].retain(|_| (!bucket_movement.contains(&i), i += 1).0);
+    i = 0;
+    spores.speeds[vert][horz].retain(|_| (!bucket_movement.contains(&i), i += 1).0);
+    i = 0;
+    spores.spore_types[vert][horz].retain(|_| (!bucket_movement.contains(&i), i += 1).0);
+}
+
+
 struct SporeBucketMovement {
-    old_bucket_index: usize,
+    index_in_old_bucket: usize,
     new_bucket_coord: (usize, usize),
     spore_data: (Vector, Vector, u8),
 }
@@ -85,7 +87,7 @@ fn calc_bucket_movements(
         let new_bucket_coord = get_bucket_from_pos(spores.positions[vert][horz][old_bucket_index]);
         if new_bucket_coord != (horz, vert) {
             bucket_movements.push(SporeBucketMovement {
-                old_bucket_index,
+                index_in_old_bucket: old_bucket_index,
                 new_bucket_coord,
                 spore_data: (
                     spores.positions[vert][horz][old_bucket_index],
